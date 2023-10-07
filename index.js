bedc57f82b7cbf6b2df687a146ef3be362d78edc
bella-t22 <b.jtorr22@gmail.com>
2023-10-06 18:15:27 -0700

    Add equation variables and operator function in index.js

--- index.js
+++ index.js
@@ -16,4 +16,21 @@ function divide(firstNum, secondNum) {
 }
 
 // create 3 variables for each part of equation (number operator number).
+const firstNum = document.querySelector('#firstNum');
+const operator = document.querySelector('#operator');
+const secondNum = document.querySelector('#secondNum');
+
 // create an operator function that takes these variables and uses one of the previous functions to solve the equation.
+function operator(firstNum, symbol, secondNum) {
+    if (symbol === '+') {
+        return add(firstNum, secondNum);
+    } else if (symbol === '-') {
+        return subtract(firstNum, secondNum);
+    } else if (symbol === 'x') {
+        return multiply(firstNum, secondNum);
+    } else if (symbol === '/') {
+        return divide(firstNum, secondNum);
+    } else {
+        return 'Uh oh, try again';
+    }
+}
