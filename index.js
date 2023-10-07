37d9dfa27be5ce22c55fdce3ee2389d022b45944
bella-t22 <b.jtorr22@gmail.com>
2023-10-06 18:03:10 -0700

    Add basic math operations functions to index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// create functions for all basic math operators. (+-*/)
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+// create 3 variables for each part of equation (number operator number).
+// create an operator function that takes these variables and uses one of the previous functions to solve the equation.
